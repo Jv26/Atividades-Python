834291e64f055147adeab656122d26d9d1acad9f
Vitafit <projeto.vitafit@gmail.com>
2020-10-05 13:30:44 -0300

    atividades feitas

---
+++ Atividade 4.py
@@ -0,0 +1,11 @@
+numero = int(input('digite um valor:'))
+number = int(input('digite um valor:'))
+
+if numero == number:
+    print('os números são iguais')
+
+if numero > number:
+    print('o primeiro valor é maior que o segundo')
+
+if numero < number:
+    print('o segundo numero é maior que o primeiro')
